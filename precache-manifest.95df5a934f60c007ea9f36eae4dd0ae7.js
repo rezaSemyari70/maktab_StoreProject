6484bada6f73d81d80d3cf4da17ea140c2c68cc2
reza <rezasemyari1991@gmail.com>
2020-09-09 12:37:25 +0430

    Updates

---
+++ precache-manifest.95df5a934f60c007ea9f36eae4dd0ae7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb2065c2fef5f0024306ecb989368bd",
+    "url": "/maktab_StoreProject/index.html"
+  },
+  {
+    "revision": "2b185e9a737f8be511c0",
+    "url": "/maktab_StoreProject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cadf9844040aae11b560",
+    "url": "/maktab_StoreProject/static/js/2.23a989f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/maktab_StoreProject/static/js/2.23a989f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b185e9a737f8be511c0",
+    "url": "/maktab_StoreProject/static/js/main.67e32a17.chunk.js"
+  },
+  {
+    "revision": "bf4aa3db86f634fab116",
+    "url": "/maktab_StoreProject/static/js/runtime-main.19f1b593.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/maktab_StoreProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
