14469cd7d22f030e43ee0478f8720621db02f8ca
reza <rezasemyari1991@gmail.com>
2020-09-22 15:47:48 +0330

    add specialproduct slider home

---
+++ src/Views/HomeSlider/HomeSlider.jsx
@@ -0,0 +1,110 @@
+import React, {useState} from 'react';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption,
+    Row,
+    Col
+} from 'reactstrap';
+
+import './HomeSlider.scss';
+
+const items = [
+    {
+        src: "https://woocommerce.maktabsharif.ir/wp-content/uploads/2020/01/3637521.jpg",
+        altText: 'Slide 1',
+        caption: 'Slide 1'
+    }, {
+        src: "https://woocommerce.maktabsharif.ir/wp-content/uploads/2020/01/2782153.jpg",
+        altText: 'Slide 2',
+        caption: 'Slide 2'
+    }, {
+        src: "https://woocommerce.maktabsharif.ir/wp-content/uploads/2020/01/3239247.jpg",
+        altText: 'Slide 3',
+        caption: 'Slide 3'
+    }, {
+        src: "https://woocommerce.maktabsharif.ir/wp-content/uploads/2019/12/2473246.jpg",
+        altText: 'Slide 4',
+        caption: 'Slide 4'
+    }, {
+        src: "https://woocommerce.maktabsharif.ir/wp-content/uploads/2020/01/113845063.jpg",
+        altText: 'Slide 5',
+        caption: 'Slide 5'
+    }, {
+        src: "https://woocommerce.maktabsharif.ir/wp-content/uploads/2019/12/2371066.jpg",
+        altText: 'Slide 6',
+        caption: 'Slide 6'
+    }
+    //   ,   {     src:
+    // "https://woocommerce.maktabsharif.ir/wp-content/uploads/2019/12/114165665.jpg"
+    // ,     altText: 'Slide 7',     caption: 'Slide 7'   }
+
+];
+
+const HomeSlider = (props) => {
+    const [activeIndex,
+        setActiveIndex] = useState(0);
+    const [animating,
+        setAnimating] = useState(false);
+
+    const next = () => {
+        if (animating) 
+            return;
+        const nextIndex = activeIndex === items.length - 1
+            ? 0
+            : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const previous = () => {
+        if (animating) 
+            return;
+        const nextIndex = activeIndex === 0
+            ? items.length - 1
+            : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const goToIndex = (newIndex) => {
+        if (animating) 
+            return;
+        setActiveIndex(newIndex);
+    }
+
+    const slides = items.map((item) => {
+        return (
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={item.src}>
+                <img src={item.src} alt={item.altText}/>
+                <CarouselCaption captionText={item.caption} captionHeader={item.caption}/>
+            </CarouselItem>
+        );
+    });
+
+    return (
+      <Row >
+        <Col></Col>
+        <Col xs={12}>
+        <Carousel
+        className="carouselBox"
+            activeIndex={activeIndex}
+            next={next}
+            previous={previous}
+          >
+            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+            {slides}
+            <CarouselControl className="rounded" direction="prev" directionText="Previous" onClickHandler={previous} />
+            <CarouselControl  direction="next" directionText="Next" onClickHandler={next} />
+      </Carousel>
+        </Col>
+        <Col></Col>
+  
+      </Row>
+    );
+}
+
+export default HomeSlider;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 render(
-    <React.StrictMode>
     <App/>
-</React.StrictMode>, document.getElementById('root'));
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls. Learn
